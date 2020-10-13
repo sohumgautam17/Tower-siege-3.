e74973dd29ab4c68d121aa70fac9c35ae5b5cdf6
sohumgautam17 <69330896+sohumgautam17@users.noreply.github.com>
2020-10-13 17:31:36 -0400

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,44 @@
+class Box  {
+    constructor(x, y, width, height){
+        var options = {
+           // isStatic:false,
+            restitution:0.8,
+            friction:0.1,
+            
+    }
+    this.visibility = 225;
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+   }
+   
+   score(){
+     if(this.body.speed>3){
+        score++;
+    }
+   }
+   display() {
+    
+        console.log(this.body.speed);
+        if(this.body.speed < 3){
+            var angle = this.body.angle;
+            var pos= this.body.position;
+            push();
+            translate(pos.x, pos.y);
+            rotate(angle);
+            rectMode(CENTER);
+            rect(0,0,this.width, this.height); 
+            pop();        
+           }
+           else{
+        World.remove(world, this.body);
+        push();
+        
+        this.visiblity = this.visiblity - 5;
+      
+        pop();
+    }
+ }  
+};
+  
